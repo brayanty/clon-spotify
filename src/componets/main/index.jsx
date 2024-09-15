1f6d3fbb1d179f876107ae3ebeea57bbaf778980
Brayan <danielsegoviad@gmail.com>
2024-09-15 02:43:11 -0500

    Add user controller preference

--- src/componets/main/index.jsx
+++ src/componets/main/index.jsx
@@ -1,17 +1,60 @@
+import { useState } from "react";
 import card from "../../assets/Cards/descarga.jfif";
+
 function Main() {
+  const [isSelecta, seSelect] = useState("Todo");
+
+  const handlerClick = (e) => {
+    const newSelect = e.target.childNodes[0].nodeValue;
+    seSelect(newSelect);
+  };
+
   return (
     <main className="Main p-2 rounded-md bg-slate-500/40 flex flex-col gap-3 overflow-hidden h-full overflow-y-scroll overscroll-x-contain snap-x snap-proximity">
       <nav>
         <ul className="flex gap-4">
           <li>
-            <a href="#">Todo</a>
+            <button
+              className={`${
+                isSelecta == "All" ? "border-gray-400" : "border-transparent"
+              }
+              border rounded-md p-1`}
+              value="all"
+              onClick={(e) => {
+                handlerClick(e);
+              }}
+            >
+              All
+            </button>
           </li>
           <li>
-            <a href="#">Música</a>
+            <button
+              className={`${
+                isSelecta == "Music" ? "border-gray-400" : "border-transparent"
+              }
+              border rounded-md p-1`}
+              value="all"
+              onClick={(e) => {
+                handlerClick(e);
+              }}
+            >
+              Music
+            </button>
           </li>
           <li>
-            <a href="#">Podcasts</a>
+            <button
+              className={`${
+                isSelecta == "Podcasts"
+                  ? "border-gray-400"
+                  : "border-transparent"
+              }
+              border rounded-md p-1`}
+              onClick={(e) => {
+                handlerClick(e);
+              }}
+            >
+              Podcasts
+            </button>
           </li>
         </ul>
       </nav>
@@ -20,7 +63,9 @@ function Main() {
           <h4>Hecho para ti</h4>
           <div>Mostrar todo</div>
         </div>
-        <div className="flex gap-2 justify-between">
+        <div className="flex gap-2 justify-between flex-wrap">
+          <RenderCard />
+          <RenderCard />
           <RenderCard />
           <RenderCard />
           <RenderCard />
@@ -32,7 +77,9 @@ function Main() {
           <h4>Hecho para ti</h4>
           <div>Mostrar todo</div>
         </div>
-        <div className="flex gap-2 justify-between">
+        <div className="flex gap-2 justify-around flex-wrap">
+          <RenderCard />
+          <RenderCard />
           <RenderCard />
           <RenderCard />
           <RenderCard />
