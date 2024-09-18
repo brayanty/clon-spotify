cb2a59181b8ece59197df7dcd8c489c8a17324b1
Brayan <danielsegoviad@gmail.com>
2024-09-17 22:39:26 -0500

    Add menu

--- src/componets/navbar/index.jsx
+++ src/componets/navbar/index.jsx
@@ -5,15 +5,44 @@ import {
   faX,
 } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useState, useEffect, useRef } from "react";
 
 function Navbar() {
+  const [navHeight, setnavHeight] = useState(" ");
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const menuRef = useRef(null);
+  const navRef = useRef(null);
+
+  const menuVisible = () => {
+    setnavHeight(navRef.current.clientHeight + "px");
+    setIsMenuOpen(true);
+  };
+
+  const closeMenu = () => setIsMenuOpen(false);
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (menuRef.current && !menuRef.current.contains(event.target)) {
+        closeMenu();
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
   return (
-    <nav className="container mx-auto flex justify-between gap-2 max-md:justify-evenly items-center px-2 pt-2">
+    <nav
+      ref={navRef}
+      className="relative container mx-auto flex justify-between gap-2 max-md:justify-evenly items-center pt-2"
+    >
       <div className="flex items-center">
         <FontAwesomeIcon icon={faSpotify} size="2xl" />
-        {/* barra de busqueda */}
+        {/* Search bar */}
       </div>
-      <div className="flex flex-row justify-center gap-1 items-center px-3 py-3 w-fullsm:w-[20rem]  md:w-[30rem] bg-gray-700/70 rounded-3xl">
+      <div className="flex flex-row justify-center gap-1 items-center px-3 py-3 w-full sm:w-[20rem] md:w-[30rem] bg-gray-700/70 rounded-3xl">
         <div>
           <FontAwesomeIcon icon={faMagnifyingGlass} size="lg" />
         </div>
@@ -24,7 +53,7 @@ function Navbar() {
             type="text"
             name="text"
             id="text"
-            placeholder="¿Qué deceas buscar?"
+            placeholder="¿Qué deseas buscar?"
           />
         </div>
         <div>
@@ -32,12 +61,36 @@ function Navbar() {
         </div>
       </div>
       <ul className="flex gap-3 justify-evenly items-center">
-        <li className="cursor-pointer border p-4 rounded-full border-gray-500">
-          <a href="#">
-            <FontAwesomeIcon icon={faUser} size="xl" />
-          </a>
+        <li
+          onClick={menuVisible}
+          className="cursor-pointer border rounded-full border-gray-500"
+        >
+          <FontAwesomeIcon className="p-4" icon={faUser} size="lg" />
         </li>
       </ul>
+      {isMenuOpen && (
+        <div
+          ref={menuRef}
+          style={{
+            position: "absolute",
+            top: navHeight,
+            right: "1rem",
+          }}
+          className="font-semibold bg-slate-500/50 backdrop-blur-sm w-32 shadow-lg rounded-md p-2"
+        >
+          <ul className=" flex flex-col ">
+            <li className="hover:bg-gray-200 rounded-md hover:text-black  cursor-pointer p-2">
+              Login
+            </li>
+            <li className="hover:bg-gray-200 rounded-md hover:text-black  cursor-pointer p-2">
+              Settings
+            </li>
+            <li className="hover:bg-gray-200 rounded-md hover:text-black  cursor-pointer p-2">
+              Premium
+            </li>
+          </ul>
+        </div>
+      )}
     </nav>
   );
 }
